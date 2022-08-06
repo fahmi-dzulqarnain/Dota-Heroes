59482ecc39b6b9af3b4ca9333e13086c08616c06
Fahmi Dzulqarnain <60492666+fahmi-dzulqarnain@users.noreply.github.com>
2022-08-06 14:44:53 +0700

    Initial Commit

---
+++ Dota Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota Heroes
+//
+//  Created by Fahmi Dzulqarnain on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
